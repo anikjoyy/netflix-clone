ce4f5e520514217a712fa399222c7a9da05d2696
anikjoyy <anikjoy.cse@gmail.com>
2023-02-14 02:51:13 +0600

    created banner section

---
+++ src/Components/Home/Promos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Promos = () => {
+  return <div></div>;
+};
+
+export default Promos;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Banner from '../Components/Home/Banner';
+import PopularMovies from '../Components/Home/PopularMovies';
+import Promos from '../Components/Home/Promos';
+import TopRated from '../Components/Home/TopRated';
 import Layout from '../Layout/Layout';
 const Home = () => {
   return (
     <div>
       <Layout>
-        <div>Home</div>
+        <div className='container mx-auto min-h-screen px-2 mb-6'>
+          <Banner />
+          <PopularMovies />
+          <Promos />
+          <TopRated />
+        </div>
       </Layout>
     </div>
   );
