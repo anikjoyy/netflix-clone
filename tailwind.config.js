86cc0edcf42c117b9dab0ddfaab68459259366d8
anikjoyy <anikjoy.cse@gmail.com>
2023-02-14 00:46:23 +0600

    installed tailwindCSS and made setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  //we start to customize colors and fonts
+  theme: {
+    extend: {
+      colors: {
+        main: '#080A1A',
+        subMain: '#F20000',
+        dry: '#0B0F29',
+        star: '#FFB000',
+        text: '#C0C0C0',
+        borser: '4B5563',
+        dryGray: 'E0D5D5',
+      },
+      height: {
+        header: '560px',
+        rate: '400px',
+      },
+      fontSize: {
+        h1: '2.6rem',
+      },
+      screens: {
+        xs: '475px',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+};
