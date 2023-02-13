070391ad9ada96b7926968689e874d218713462a
anikjoyy <anikjoy.cse@gmail.com>
2023-02-14 00:16:28 +0600

    initialized project and made fake data

---
+++ Data/CategoriesData.js
@@ -0,0 +1,58 @@
+export const CategoriesDate = [
+  {
+    _id: 1,
+    title: 'Romantic',
+  },
+  {
+    _id: 2,
+    title: 'Action',
+  },
+  {
+    _id: 3,
+    title: 'Horror',
+  },
+  {
+    _id: 4,
+    title: 'Comedy',
+  },
+  {
+    _id: 5,
+    title: 'Adventure',
+  },
+  {
+    _id: 6,
+    title: 'Sports',
+  },
+  {
+    _id: 7,
+    title: 'Musicals',
+  },
+  {
+    _id: 8,
+    title: 'Drama',
+  },
+  {
+    _id: 9,
+    title: 'Thriller',
+  },
+  {
+    _id: 10,
+    title: 'Fantasy',
+  },
+  {
+    _id: 11,
+    title: 'Western',
+  },
+  {
+    _id: 12,
+    title: 'Historical',
+  },
+  {
+    _id: 13,
+    title: 'Science Fiction',
+  },
+  {
+    _id: 14,
+    title: 'Mystery',
+  },
+];
